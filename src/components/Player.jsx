10886c20bf1b4ffc2284f40498f3b2f97ce77191
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-05 11:22:24 +0200

    feat: editing name done

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 
 export default function Player({name, symbol}) {
+  const [playerName, setName] = useState(name)
   const [ isEditing, setEditing ] = useState(false);
 
   // console.log(isEdit);
   
   function handleButton(){
-    setEditing(true)
+    setEditing(editing => !editing)
+  }
+
+  function handleChangeName(event) {
+    setName(event.target.value);
   }
   
-  let editPlayer = <span className="player-name"> {name}</span> 
-              
+  let editPlayer = <span className="player-name"> {playerName}</span> 
+  let btnCaption = "Edit"
+
   if (isEditing) {
-    editPlayer = <input type="text" required />
+    editPlayer = <input type="text" required value={playerName} onChange={handleChangeName} />
+    btnCaption="Save"
   }
   
     return (
@@ -21,7 +28,7 @@ export default function Player({name, symbol}) {
               {editPlayer}
               <span className="player-symbol"> {symbol} </span> 
             </span>
-            <button onClick={() => handleButton(true)}>Edit</button>
+            <button onClick={handleButton}>{btnCaption}</button>
         </li>
     )
 
