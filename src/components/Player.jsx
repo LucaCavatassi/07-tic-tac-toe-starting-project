02b234d5f0b3232b38b48b5d94875eeeecc2cdb2
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-05 10:30:55 +0200

    feat: change to input based on edit click

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+  const [ isEditing, setEditing ] = useState(false);
+
+  // console.log(isEdit);
+  
+  function handleButton(bool){
+    setEditing(bool)
+  }
+  
+  console.log(isEditing);
+
+  if (isEditing === true) {
+    return (
+      <li>
+            <span className="player">
+              <input type="text" />
+              <span className="player-symbol"> {symbol} </span> 
+            </span>
+            <button onClick={() => handleButton(true)}>Edit</button>
+      </li>
+    )
+  } else {
     return (
         <li>
             <span className="player">
               <span className="player-name"> {name}</span> 
               <span className="player-symbol"> {symbol} </span> 
             </span>
-            <button>Edit</button>
-         </li>
+            <button onClick={() => handleButton(true)}>Edit</button>
+        </li>
     )
+  }
+
 }
